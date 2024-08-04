51623e871798488591bd81a248bb0a0af83b5234
ankit-mallik <hritikmallik@gmail.com>
2024-08-04 15:47:20 +0545

    Initial commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+// 1) Middleware
+// console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+app.use(express.json()); // middleware app.use to use middle ware
+app.use(express.static(`${__dirname}/public`));
+
+// app.use((req, res, next) => {
+//   console.log('Hello from the middleware 👋');
+//   next();
+// });
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  // console.log(req.headers);
+  next();
+});
+
+//Route handlers
+
+//Routes
+// app.get('/api/v1/tours', getAllTours);
+// app.get('/api/v1/tours/:id', getTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.post('/api/v1/tours', createTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+// start server
+module.exports = app;
