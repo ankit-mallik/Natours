bf3f42d8c9bc9156640309737d9e51e0d354ae35
ankit-mallik <hritikmallik@gmail.com>
2024-08-05 13:58:59 +0545

    added jwt cookie functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,11 +9,20 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) Middleware
+// 1) global Middleware
 // console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try again in an hour!'
+});
+
+app.use('/api', limiter);
+
 app.use(express.json()); // middleware app.use to use middle ware
 app.use(express.static(`${__dirname}/public`));
 
