c6794dba82ae5f685b38b8f51302e723acc3c2f7
ankit-mallik <hritikmallik@gmail.com>
2024-08-05 15:23:23 +0545

    prevent parameter pollution

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -38,6 +39,20 @@ app.use(express.json({ limit: '10kb' })); // middleware app.use to use middle wa
 app.use(mongoSanitize());
 // data sanitization against XXS
 app.use(xss());
+
+// prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
 // serving static files
 app.use(express.static(`${__dirname}/public`));
 
